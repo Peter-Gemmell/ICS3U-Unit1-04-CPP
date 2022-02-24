745be2eb6b246e83c659ff8690babdf1d92a2315
Peter Gemmell <peter.gemmell@mths.ca>
2022-02-24 19:56:25 +0000

    zamn

---
+++ area_Perimeter.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2020 Peter Gemmell All rights reserved
+//
+// Created by Peter Gemmell
+// Created on February 2022
+// This is the Perimeter and Area for a rectangle of 5 x 3 dimensions
+
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has the dimensions: " << std::endl;
+    std::cout << "5cm x 3cm" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Area is " << (5 * 3) << "cm2." << std::endl;
+    std::cout << "Perimeter is " << (2 * (5 + 3)) << "cm." << std::endl;
+}
